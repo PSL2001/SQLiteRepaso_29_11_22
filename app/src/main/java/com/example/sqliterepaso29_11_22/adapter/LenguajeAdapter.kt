4a8e4996e310896583dd50812fbddcc521b5d98f
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2022-11-29 13:56:27 +0100

    Creado adapter y viewholder con layouts correspondiente

---
+++ app/src/main/java/com/example/sqliterepaso29_11_22/adapter/LenguajeAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.sqliterepaso29_11_22.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sqliterepaso29_11_22.R
+import com.example.sqliterepaso29_11_22.modelos.Lenguajes
+
+class LenguajeAdapter(private var lista:MutableList<Lenguajes>): RecyclerView.Adapter<LenguajeViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LenguajeViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.layout_lenguajes, parent, false)
+        return LenguajeViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: LenguajeViewHolder, position: Int) {
+        val lenguaje = lista[position]
+        holder.render(lenguaje)
+    }
+
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+}
---
+++ app/src/main/java/com/example/sqliterepaso29_11_22/adapter/LenguajeViewHolder.kt
@@ -0,0 +1,15 @@
+package com.example.sqliterepaso29_11_22.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sqliterepaso29_11_22.databinding.LayoutLenguajesBinding
+import com.example.sqliterepaso29_11_22.modelos.Lenguajes
+
+class LenguajeViewHolder(v: View): RecyclerView.ViewHolder(v) {
+    val binding = LayoutLenguajesBinding.bind(v)
+
+    fun render(lenguaje: Lenguajes) {
+        binding.tvNombre.text = lenguaje.nombre
+        binding.tvDif.text = lenguaje.dificultad
+    }
+}
