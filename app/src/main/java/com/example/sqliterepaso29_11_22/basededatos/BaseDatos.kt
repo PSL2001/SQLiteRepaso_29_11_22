c6c288c09f9e44d5ea971152b328eeba9209b81d
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2022-11-29 13:26:12 +0100

    Creado metodo create y read

--- app/src/main/java/com/example/sqliterepaso29_11_22/basededatos/BaseDatos.kt
+++ app/src/main/java/com/example/sqliterepaso29_11_22/basededatos/BaseDatos.kt
@@ -1,8 +1,10 @@
 package com.example.sqliterepaso29_11_22.basededatos
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.sqliterepaso29_11_22.modelos.Lenguajes
 
 class BaseDatos(c: Context): SQLiteOpenHelper(c, DB, null, VERSION) {
     companion object {
@@ -26,7 +28,39 @@ class BaseDatos(c: Context): SQLiteOpenHelper(c, DB, null, VERSION) {
     }
 
     //Metodos para gestionar la base de datos (CRUD)
-    fun create() {
-
+    fun create(lenguaje: Lenguajes): Long {
+        val con = this.writableDatabase
+        val valores = ContentValues().apply {
+            put("nombre", lenguaje.nombre)
+            put("dificultad", lenguaje.dificultad)
+        }
+        //Ins devuelve -1 si ha habido errores
+        val ins = con.insert(TABLA, null, valores)
+        con.close()
+        return ins
+    }
+    //Funcion de leer
+    fun read(): MutableList<Lenguajes> {
+        val lista = mutableListOf<Lenguajes>()
+        val q = "SELECT * FROM $TABLA order by dificultad"
+        val con = this.readableDatabase
+        try {
+            val cursor = con.rawQuery(q, null)
+            if (cursor.moveToFirst()) {
+                do {
+                    val lenguajes = Lenguajes(
+                        cursor.getInt(0), //cursor.getColumnIndex("id")
+                        cursor.getString(1),
+                        cursor.getString(2)
+                    )
+                    lista.add(lenguajes)
+                }while (cursor.moveToNext())
+            }
+            cursor.close()
+        }catch (e: Exception) {
+            e.printStackTrace()
+        }
+        con.close()
+        return lista
     }
 }
